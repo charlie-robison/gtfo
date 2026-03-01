6f59f79d59084511d5c2a4cdb9802a301c2fed63
charlie-robison <charlierobison480@gmail.com>
2026-02-28 16:05:32 -0800

    Reimplemented api.

--- server/convex/convex.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineApp } from "convex/server";
-import browserUse from "browser-use-convex-component/convex.config.js";
-
-const app = defineApp();
-app.use(browserUse);
-
-export default app;
